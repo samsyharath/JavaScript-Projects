9fe1523cc5f5a4b8c7c3a3c044302c21101ffaa1
Samson Syharath <samsonsyharath@gmail.com>
2023-09-14 13:05:47 -0700

    Added JavaScript to index.html and linked to js

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+var A = "This is a message.";
+var stringOne = "This is a \"string\"."
+var stringTwo = "Yes." + " This is a second string."
+var food = "Lagasna", appetizer = "Spring Rolls", entree = "Pad Thai", dessert = "Mango Rice"
+
+function My_First_Function () { // defines a function and names it
+    var str = "This is the button text!"; // Defines a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str; // Puts the value of the variable into the HTML elementFromPoint with the "Button_Text" id
+}
+
+var Sent1 = "This is the beginning of the string" // Defines a variable and giving it a string value
+var Sent2 = " and this is the end of the string" // Defines a variable and giving it a string value
+
+
+window.alert(stringOne) //Displays a popup window
+
+
+
+document.write(Sent1+Sent2) //Displays the concatenated statement
