482cd22cf118d340aaf04b129b2a8a04e090a350
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2023-10-28 16:05:50 -0700

    Updated checkWinConditions()

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -25,8 +25,8 @@ function placeXOrO(squareNumber) {
         //This calls a function to check for any win conditions.
         checkWinConditions();
         //This condition is for changing the active player.
-        if (activePlayer == 'X') {
-            //If actove player is 'X' change it to 'O'.
+        if (activePlayer === 'X') {
+            //If active player is 'X' change it to 'O'.
             activePlayer = 'O';
             //if active player is anthing other than 'X'
         } else {
@@ -34,7 +34,7 @@ function placeXOrO(squareNumber) {
             activePlayer = 'X';
         }
         //This function plays placement sound.
-        Audio('./media/place.mp3');
+        audio('./media/place.mp3');
         //This condition checks to see if it is the computers turn.
         if (activePlayer === 'O') {
             //This function diables clicking for the computers turn.
@@ -54,7 +54,7 @@ function placeXOrO(squareNumber) {
         //This condition allows our while loop to keep trying if a square is selected already.
         while (!success) {
             //A random number between 0 and 8 is selected.
-            pickASquare = "String(Math.floor(Math.random() * 9))";
+            pickASquare = String(Math.floor(Math.random() * 9));
             //If the random number evaluated returns true, the square hasn't been selected yet.
             if (placeXOrO(pickASquare)) {
                 //This line calls the function.
@@ -67,4 +67,32 @@ function placeXOrO(squareNumber) {
 }
 
 //This function parses the selectedSquares array to search for win conditions.
-//drawLine() function 
+//drawLine() function is called to draw a line on the screen if the condition is met.
+function checkWinConditions() {
+    //X O, 1, 2 condition.
+    if (arrayIncludes('OX', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
+    // X 3, 4, 5 condition.
+    else if (arrayIncludes( '3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304) }
+    //X 6, 7, 8 condition.
+    else if (arrayIncludes( '6X', '7X', '8X')) {drawWinLine(50, 508, 558, 508) }
+    //
+    else if (arrayIncludes( '0X', '4X', '7X')) {drawWinLine(50, 304, 558, 304) }
+    //
+    else if (arrayIncludes( '1X', '4X', '7X')) {drawWinLine(304, 50, 304, 558) }
+    //
+    else if (arrayIncludes( '2X', '5X', '8X')) {drawWinLine(508, 50, 508, 558) }
+    //
+    else if (arrayIncludes( '3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304) }
+    //
+    else if (arrayIncludes( '0X', '4X', '8X')) {drawWinLine(100, 100, 520, 520) }
+    //
+    else if (arrayIncludes( '0O', '1O', '2O')) {drawWinLine(50, 100, 558, 100) }
+    //
+    else if (arrayIncludes( '3O', '4O', '5O')) {drawWinLine(50, 304, 558, 304) }
+    //
+    else if (arrayIncludes( '6O', '7O', '8O')) {drawWinLine(50, 508, 558, 508) }
+    //
+    else if (arrayIncludes( '0O', '3O', '6O')) {drawWinLine(100, 50, 100, 558) }
+    //
+    else if (arrayIncludes( '3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304) }
+}
